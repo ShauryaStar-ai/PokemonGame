d7cbb6c966ac34e6bbe26ded6263e63869a2e490
ShauryaStar-ai <shauryahooda2009@gmail.com>
2025-07-06 11:29:04 -0400

    dede

--- Code/src/Interfaces/PokemonForPlayer.java
+++ Code/src/Interfaces/PokemonForPlayer.java
@@ -2,5 +2,6 @@ package Interfaces;
 
 public interface PokemonForPlayer {
     String attack();
+
 }
 
--- Code/src/PlayingGame/ControlFlow.java
+++ Code/src/PlayingGame/ControlFlow.java
@@ -4,9 +4,7 @@ import Pokemon.*;
 
 public class ControlFlow {
     public static void main(String[] args) {
-        Bulbasour bulbasour = new Bulbasour();
 
         Player ram = new Player("Ram",10);
-        System.out.println(bulbasour);
     }
 }
